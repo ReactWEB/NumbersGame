4a578da94bdac5ec49e6716ca478e3d3b799c4d3
ReactWEB <reactweb@ukr.net>
2021-09-30 18:12:40 +0300

    why don't work?

--- js/main.js
+++ js/main.js
@@ -1,23 +1,31 @@
-let first = prompt("Please, enter your first number:", );
-let second = prompt("Please, enter your second number:", );
-
 const evenMassive = []; //четные числа
 const oddMassive = []; // нечетные числа
 
-const game = () => {
-  let x = first % second;  
-    while (evenMassive.length < 6 || oddMassive < 6) {
-      if (x == 0) {
-        evenMassive.push(x);
-      } else {
-        oddMassive.push(x);
-      }      
-    }
-    if (evenMassive.length === 5) {
-      alert("The even wins!");
-    } else if(oddMassive.length === 5){
-      alert("The odd wins!");
-    }  
-}
+const askArr = [
+  ['Please enter your first number:', ],
+  ['Please enter your second number:', ]
+];
 
-game();
+const game = (quest) => {
+  let first = prompt(askArr[0], '');
+  let second = prompt(askArr[1], '');
+  let x = (first + second) % 2;
+  do {
+    if (x == 0) {
+      evenMassive.push(x);
+    } else {
+      oddMassive.push(x);
+    }
+  } while (evenMassive.length > 0 && evenMassive.length < 5 || oddMassive.length > 0 && oddMassive.length < 5) {
+    for (var i = 0; i < askArr.length; i++) {
+      game(quest[i]);
+    }
+    evenMassive.length++;
+    oddMassive.length++;
+    if (evenMassive.length === 4) {
+      alert("The even wins!");      
+    } else if (oddMassive.length === 4) {
+      alert("The odd wins!");
+    }
+  }
+}
