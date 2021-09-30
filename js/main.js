d2849d174aac930057a5a6df0f03a052a2ba6774
ReactWEB <reactweb@ukr.net>
2021-09-30 14:54:11 +0300

    don't want working

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-let first = prompt("Please, enter your first number:", []);
-let second = prompt("Please, enter your second number:", []);
+let first = prompt("Please, enter your first number:", );
+let second = prompt("Please, enter your second number:", );
 
 const evenMassive = []; //четные числа
 const oddMassive = []; // нечетные числа
 
 const game = () => {
-  let x = first % second;
-  if (first !== String && second !== String) {
+  let x = first % second;  
     while (evenMassive.length < 6 || oddMassive < 6) {
       if (x == 0) {
         evenMassive.push(x);
@@ -16,12 +15,9 @@ const game = () => {
     }
     if (evenMassive.length === 5) {
       alert("The even wins!");
-    } else {
+    } else if(oddMassive.length === 5){
       alert("The odd wins!");
-    }
-  } else {
-    alert("Please, you need typing number only!");
-  }
+    }  
 }
 
-game(10);
+game();
