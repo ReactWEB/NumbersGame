e137ed948fc0fb964ba47ecb6bd58ec42d74c5f6
ReactWEB <reactweb@ukr.net>
2021-09-30 22:02:04 +0300

    incorrect job

--- js/main.js
+++ js/main.js
@@ -1,31 +1,25 @@
 const evenMassive = []; //четные числа
 const oddMassive = []; // нечетные числа
 
-const askArr = [
-  ['Please enter your first number:', ],
-  ['Please enter your second number:', ]
-];
-
-const game = (quest) => {
-  let first = prompt(askArr[0], '');
-  let second = prompt(askArr[1], '');
-  let x = (first + second) % 2;
+const game = () => {
   do {
-    if (x == 0) {
+    let first = parseInt(prompt('Enter your first number:', ''));
+    let second = parseInt(prompt('Enter your second number:', ''));
+    let x = first + second;
+    if (x % 2 === 0) {
       evenMassive.push(x);
     } else {
       oddMassive.push(x);
     }
-  } while (evenMassive.length > 0 && evenMassive.length < 5 || oddMassive.length > 0 && oddMassive.length < 5) {
-    for (var i = 0; i < askArr.length; i++) {
-      game(quest[i]);
-    }
-    evenMassive.length++;
-    oddMassive.length++;
-    if (evenMassive.length === 4) {
-      alert("The even wins!");      
-    } else if (oddMassive.length === 4) {
-      alert("The odd wins!");
+  } while (evenMassive.length > 0 && evenMassive.length < 3 || oddMassive.length > 0 && oddMassive.length < 3) {
+    if (evenMassive.length === 2) {
+      console.log("The even wins!");
+    } else if (oddMassive.length === 2) {
+      console.log("The odd wins!");
     }
   }
 }
+
+game();
+console.log(evenMassive);
+console.log(oddMassive);
